f545f3c2e39dc0cf95ae141fe3e72e311a373874
Shawn East <alphadog2425@yahoo.com>
2026-08-20 23:24:47 -0500

    new updated code

--- movie.js
+++ movie.js
@@ -1,2 +1,27 @@
-//http://www.omdbapi.com/?apikey=b8294bb2&
+//http://www.omdbapi.com/?apikey=b8294bb2&s=
 //http://img.omdbapi.com/?apikey=b8294bb2&
+
+
+const movieList = document.querySelector(".movie__list");
+
+async function scream() {
+
+    const response = await fetch(
+        `http://www.omdbapi.com/?apikey=b8294bb2&s=scream`
+    );
+
+    const data = await response.json();
+
+    movieList.innerHTML = data.Search.map(movie => {
+        return `
+            <div class="movie">
+                <img src="${movie.Poster}" class="movie__img">
+                <h2>${movie.Title}</h2>
+                <p>${movie.Year}</p>
+            </div>
+        `;
+    }).join("");
+
+}
+
+scream();
