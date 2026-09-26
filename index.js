a7cfc1091f7ae3aa349b7d616376841dd33982a6
Shawn East <alphadog2425@yahoo.com>
2026-09-26 13:54:24 -0500

    touching up

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const searchInput = document.querySelector("#searchInput");
 const searchButton = document.querySelector(".search__button");
+const menuOpen = document.querySelector(".menu__open");
+const menuClose = document.querySelector(".menu__close");
+const menu = document.querySelector(".burger");
+
+menuOpen.addEventListener("click", function () {
+    menu.classList.add("active");
+    console.log('button works')
+});
+
+menuClose.addEventListener("click", function () {
+    menu.classList.remove("active");
+    console.log('button works')
+});
 
 function goToMoviePage() {
     const movieName = searchInput.value.trim();
